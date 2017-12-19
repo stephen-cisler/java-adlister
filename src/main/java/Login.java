3429acb5d3e080c9316c84edeafd3ee9f7593d54
stephencisler <stephencisler@gmail.com>
2017-12-19 09:34:46 -0600

    temp commit

---
+++ src/main/java/Login.java
@@ -0,0 +1,13 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+public class Login {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            response.getWriter().println("<h1>LOGIN!</h1>");
+        }
+
+    }
